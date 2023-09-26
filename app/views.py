ee4dd4a556bfacfc5f67da6769c0fac7cc567988
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-09-26 11:36:55 -0400

    Upload feature (#2)
    * Flask app, DB, Login/Register & basic metadata upload and index endpoint created
    
    * Progress on upload - multiple file upload
    
    * Edit metadata endpoint setup, form fields populating
    
    * Edit metadata endpoint - route functioning to edit and connected to button/form
    
    * Delete single track form/button & route complete
    
    * Bulk action route - bulk delete functionality
    
    * Bulk edit html - individual and global edit forms
    
    * Functionality for individual bulk edit complete
    
    * Functionality for global bulk edit complete
    
    * Table styling to display tracks and clear button to clear selection
    
    * Update select to allow draggable select and removed unneeded routes

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, redirect, url_for, flash
 from . import app, db, login_manager
-from .models import User, Track
+from .models import User, Track, Artist
 from flask_login import login_user, logout_user, login_required, current_user
-from .forms import LoginForm, RegistrationForm, UploadForm
+from .forms import LoginForm, RegistrationForm, UploadForm, EditMetadataForm
 import eyed3
 import os
 import tempfile
@@ -11,8 +11,8 @@ import tempfile
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/')
 @app.route('/index')
+@app.route('/')
 def index():
     if current_user.is_authenticated:
         tracks = Track.query.filter_by(user_id=current_user.id).all()
@@ -27,7 +27,7 @@ def login():
     
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter(User.username.ilike(form.username.data)).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
@@ -50,12 +50,18 @@ def register():
     
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.password = form.password.data
-        db.session.add(user)
-        db.session.commit()
-        flash('You are now registered!')
-        return redirect(url_for('login'))
+        user = User.query.filter(User.username.ilike(form.username.data)).first()
+        if not user:
+            user = User(username=form.username.data, email=form.email.data)
+            user.password = form.password.data
+            db.session.add(user)
+            db.session.commit()
+            flash('You are now registered!')
+            return redirect(url_for('login'))
+        else:
+            print('login hello')
+            flash('Username is not unique', 'error')
+            return redirect(url_for('login'))
     
     return render_template('register.html', title='Register', form=form)
 
@@ -69,27 +75,128 @@ def allowed_file(filename):
 def upload():
     form = UploadForm()
     if form.validate_on_submit():
-        file = form.file.data
+        files = request.files.getlist('files')
+        
+        for file in files:
+            _, tmp_filename = tempfile.mkstemp()
+            file.save(tmp_filename)
 
-        _, tmp_filename = tempfile.mkstemp()
-        file.save(tmp_filename)
+            metadata = eyed3.load(tmp_filename)
 
-        metadata = eyed3.load(tmp_filename)
+            title = metadata.tag.title if metadata.tag.title else 'Unknown Title'
+            artist_name = metadata.tag.artist if metadata.tag.artist else 'Unknown Artist'
+            album = metadata.tag.album if metadata.tag.album else 'Unknown Album'
+            genre = metadata.tag.genre if metadata.tag.genre else 'Unknown Genre'
 
-        title = metadata.tag.title if metadata.tag.title else 'Unknown Title'
-        artist = metadata.tag.artist if metadata.tag.artist else 'Unknown Artist'
-        album = metadata.tag.album if metadata.tag.album else 'Unknown Album'
-        genre = metadata.tag.genre if metadata.tag.genre else 'Unknown Genre'
+            artist = Artist.query.filter(Artist.name.ilike(artist_name)).first()
+            if not artist:
+                artist = Artist(name=artist_name)
+                db.session.add(artist)
+                db.session.flush()
 
-        os.remove(tmp_filename)
+            if title != 'Unknown Title':
+                existing_track = Track.query.filter_by(title=title, artist_id=artist.id).first()
+                if existing_track:
+                    os.remove(tmp_filename) 
+                    continue
 
-        track = Track(title=title, artist=artist, album=album, genre=genre, user_id=current_user.id)
-        db.session.add(track)
+            os.remove(tmp_filename)
+
+            track = Track(title=title, artist_id=artist.id, album=album, genre=genre, user_id=current_user.id)
+            db.session.add(track)
+
         db.session.commit()
 
-        flash('Track uploaded successfully!', 'success')
+        flash(f'{len(files)} track(s) uploaded successfully!', 'success')
         return redirect(url_for('index'))
 
-    return render_template('upload.html', form=form)
+    return render_template('upload.html', title='Upload', form=form)
+
+@app.route('/bulk_action', methods=['GET', 'POST'])
+@login_required
+def bulk_action():
+
+    selected_tracks = request.form.getlist('selected_tracks')
+    action = request.form.get('action')
+
+    tracks = Track.query.filter(Track.id.in_(selected_tracks)).all()
+
+    if action == "Delete":
+        for track in tracks:
+            if track.user_id != current_user.id:
+                flash('You do not have permission to delete tracks.', 'danger')
+                return redirect(url_for('index'))
+            db.session.delete(track)
+        db.session.commit()
+        flash(f'{len(tracks)} tracks deleted successfully!', 'success')
+        return redirect(url_for('index'))
+
+    elif action == "Edit":
+        track_ids = request.form.getlist('selected_tracks')
+        tracks = Track.query.filter(Track.id.in_(track_ids)).all()
+        return render_template('bulk_edit.html', title='Edit Tracks', tracks=tracks)
+    else:
+        return redirect(url_for('index'))
 
 
+@app.route('/process_bulk_edit', methods=['POST'])
+@login_required
+def process_bulk_edit():
+    track_ids = request.form.getlist('track_ids')
+
+    tracks = Track.query.filter(Track.id.in_(track_ids), Track.user_id == current_user.id).all()
+
+    for track in tracks:
+        title_field = f"title_{track.id}"
+        artist_field = f"artist_{track.id}"
+        album_field = f"album_{track.id}"
+        genre_field = f"genre_{track.id}"
+
+        if title_field in request.form:
+            track.title = request.form[title_field]
+        if album_field in request.form:
+            track.album = request.form[album_field]
+        if genre_field in request.form:
+            track.genre = request.form[genre_field]
+        if artist_field in request.form:
+            artist_name = request.form[artist_field]
+            artist = Artist.query.filter(Artist.name.ilike(artist_name)).first()
+            if not artist:
+                artist = Artist(name=artist_name)
+                db.session.add(artist)
+                db.session.flush()
+            track.artist_id = artist.id
+
+    db.session.commit()
+    flash(f'{len(tracks)} tracks updated successfully!', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/process_global_bulk_edit', methods=['POST'])
+@login_required
+def process_global_bulk_edit():
+    track_ids = request.form.getlist('track_ids')
+
+    tracks = Track.query.filter(Track.id.in_(track_ids), Track.user_id == current_user.id).all()
+
+    global_album = request.form.get('global_album')
+    global_genre = request.form.get('global_genre')
+    global_artist = request.form.get('global_artist')
+
+    for track in tracks:
+        if global_album:
+            track.album = global_album
+        if global_genre:
+            track.genre = global_genre
+        if global_artist:
+            artist_name = global_artist
+            artist = Artist.query.filter(Artist.name.ilike(artist_name)).first()
+            if not artist:
+                artist = Artist(name=artist_name)
+                db.session.add(artist)
+                db.session.flush()
+            track.artist_id = artist.id
+
+    db.session.commit()
+    flash(f'{len(tracks)} tracks updated successfully!', 'success')
+    return redirect(url_for('index'))
