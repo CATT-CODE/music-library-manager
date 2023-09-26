23ae14d5cbb8b018641bf67b2c5c6c0777538b6b
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-09-26 02:29:41 -0400

    Migrating to Postgres DB and fixed .env and config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+ALLOWED_EXTENSIONS = {'mp3', 'wav', 'flac'}
+
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost') 
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+SQLALCHEMY_DATABASE_URI = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}'
+
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
+
